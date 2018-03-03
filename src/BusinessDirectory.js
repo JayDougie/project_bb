e7ac63f991805d7389c3e41f00f53030bd2fc4c7
Jeff <jeff.beauplan@bison.howard.edu>
2018-03-03 14:53:23 -0500

    added presentation

--- src/BusinessDirectory.js
+++ src/BusinessDirectory.js
@@ -11,479 +11,480 @@ import {Grid, Row, Col} from 'react-bootstrap';
 class BusinessDirectory extends Component {
   	constructor(props) {
 		super(props);
-		const json = {"ok": true,
-		"data": [{
-			"zipcode": null,
-			"website": null,
-			"state": "Washington DC",
-			"phone_number": null,
-			"name": "Supreme Discount Printing",
-			"location": {
-				"longitude": null,
-				"latitude": null
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
+		const json = {
+			"ok": true,
+			"data": [{
+				"zipcode": null,
+				"website": null,
+				"state": "Washington DC",
+				"phone_number": null,
+				"name": "Supreme Discount Printing",
+				"location": {
+					"longitude": null,
+					"latitude": null
 				},
-				"tuesday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"thursday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Washington DC",
+				"category": "Publishing",
+				"address_line_2": null,
+				"address_line_1": "5031 Bass Pl Se #204"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "DC",
+				"phone_number": null,
+				"name": "Administrative Consultant",
+				"location": {
+					"longitude": "-76.9283402",
+					"latitude": "38.8813035"
 				},
-				"sunday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"saturday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Washington DC",
+				"category": "Business Consultant",
+				"address_line_2": null,
+				"address_line_1": "5115 F Street SE"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "MD",
+				"phone_number": null,
+				"name": "Tim Weeden Web Developer/SEO",
+				"location": {
+					"longitude": "-77.2215794",
+					"latitude": "39.1190875"
 				},
-				"monday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Washington DC",
-			"category": "Publishing",
-			"address_line_2": null,
-			"address_line_1": "5031 Bass Pl Se #204"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "DC",
-			"phone_number": null,
-			"name": "Administrative Consultant",
-			"location": {
-				"longitude": "-76.9283402",
-				"latitude": "38.8813035"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Gaithersburg",
+				"category": "Developer",
+				"address_line_2": null,
+				"address_line_1": "135 Timberbrook Ln"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "DC",
+				"phone_number": null,
+				"name": "Waynette Loveness the Realtor",
+				"location": {
+					"longitude": "-76.9986306",
+					"latitude": "38.8928451"
 				},
-				"tuesday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"thursday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Washington DC",
+				"category": "Real Estate",
+				"address_line_2": null,
+				"address_line_1": "519 c street ne"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "NJ",
+				"phone_number": null,
+				"name": "Lauren M Lee, Insurence Agent",
+				"location": {
+					"longitude": "-74.17269019999999",
+					"latitude": "40.7565525"
 				},
-				"sunday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"saturday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Newark",
+				"category": "Insurence",
+				"address_line_2": null,
+				"address_line_1": "155 Summer Ave"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "MD",
+				"phone_number": null,
+				"name": "Capital Imaging, LLC",
+				"location": {
+					"longitude": "-77.1004447",
+					"latitude": "38.9897299"
 				},
-				"monday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Washington DC",
-			"category": "Business Consultant",
-			"address_line_2": null,
-			"address_line_1": "5115 F Street SE"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "MD",
-			"phone_number": null,
-			"name": "Tim Weeden Web Developer/SEO",
-			"location": {
-				"longitude": "-77.2215794",
-				"latitude": "39.1190875"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Bethesda",
+				"category": "Health Awareness",
+				"address_line_2": null,
+				"address_line_1": "4927 Auburn Avenue Suite T-25"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "VA",
+				"phone_number": null,
+				"name": "Johann's Haritique",
+				"location": {
+					"longitude": "-77.19072899999999",
+					"latitude": "38.780935"
 				},
-				"tuesday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"thursday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Springfield",
+				"category": "Beauty",
+				"address_line_2": null,
+				"address_line_1": "7233 Commerce Street"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "MD",
+				"phone_number": null,
+				"name": "Beautys Own Beauty Supply",
+				"location": {
+					"longitude": "-76.99317119999999",
+					"latitude": "38.725637"
 				},
-				"sunday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"saturday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Fort Washington",
+				"category": "Beauty",
+				"address_line_2": null,
+				"address_line_1": "922 E. Swan Creek Road"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "VA",
+				"phone_number": null,
+				"name": "Others Training Institute",
+				"location": {
+					"longitude": "-77.0896546",
+					"latitude": "38.7531305"
 				},
-				"monday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Gaithersburg",
-			"category": "Developer",
-			"address_line_2": null,
-			"address_line_1": "135 Timberbrook Ln"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "DC",
-			"phone_number": null,
-			"name": "Waynette Loveness the Realtor",
-			"location": {
-				"longitude": "-76.9986306",
-				"latitude": "38.8928451"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Alexandria",
+				"category": "Education",
+				"address_line_2": null,
+				"address_line_1": "7574 Great Swan Court"
+			}, {
+				"zipcode": null,
+				"website": null,
+				"state": "DE",
+				"phone_number": null,
+				"name": "Miss",
+				"location": {
+					"longitude": "-75.7198195",
+					"latitude": "39.6067098"
 				},
-				"tuesday": {
-					"open": null,
-					"close": null
+				"hours": {
+					"wednesday": {
+						"open": null,
+						"close": null
+					},
+					"tuesday": {
+						"open": null,
+						"close": null
+					},
+					"thursday": {
+						"open": null,
+						"close": null
+					},
+					"sunday": {
+						"open": null,
+						"close": null
+					},
+					"saturday": {
+						"open": null,
+						"close": null
+					},
+					"monday": {
+						"open": null,
+						"close": null
+					},
+					"friday": {
+						"open": null,
+						"close": null
+					}
 				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Washington DC",
-			"category": "Real Estate",
-			"address_line_2": null,
-			"address_line_1": "519 c street ne"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "NJ",
-			"phone_number": null,
-			"name": "Lauren M Lee, Insurence Agent",
-			"location": {
-				"longitude": "-74.17269019999999",
-				"latitude": "40.7565525"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Newark",
-			"category": "Insurence",
-			"address_line_2": null,
-			"address_line_1": "155 Summer Ave"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "MD",
-			"phone_number": null,
-			"name": "Capital Imaging, LLC",
-			"location": {
-				"longitude": "-77.1004447",
-				"latitude": "38.9897299"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Bethesda",
-			"category": "Health Awareness",
-			"address_line_2": null,
-			"address_line_1": "4927 Auburn Avenue Suite T-25"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "VA",
-			"phone_number": null,
-			"name": "Johann's Haritique",
-			"location": {
-				"longitude": "-77.19072899999999",
-				"latitude": "38.780935"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Springfield",
-			"category": "Beauty",
-			"address_line_2": null,
-			"address_line_1": "7233 Commerce Street"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "MD",
-			"phone_number": null,
-			"name": "Beautys Own Beauty Supply",
-			"location": {
-				"longitude": "-76.99317119999999",
-				"latitude": "38.725637"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Fort Washington",
-			"category": "Beauty",
-			"address_line_2": null,
-			"address_line_1": "922 E. Swan Creek Road"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "VA",
-			"phone_number": null,
-			"name": "Others Training Institute",
-			"location": {
-				"longitude": "-77.0896546",
-				"latitude": "38.7531305"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Alexandria",
-			"category": "Education",
-			"address_line_2": null,
-			"address_line_1": "7574 Great Swan Court"
-		}, {
-			"zipcode": null,
-			"website": null,
-			"state": "DE",
-			"phone_number": null,
-			"name": "Miss",
-			"location": {
-				"longitude": "-75.7198195",
-				"latitude": "39.6067098"
-			},
-			"hours": {
-				"wednesday": {
-					"open": null,
-					"close": null
-				},
-				"tuesday": {
-					"open": null,
-					"close": null
-				},
-				"thursday": {
-					"open": null,
-					"close": null
-				},
-				"sunday": {
-					"open": null,
-					"close": null
-				},
-				"saturday": {
-					"open": null,
-					"close": null
-				},
-				"monday": {
-					"open": null,
-					"close": null
-				},
-				"friday": {
-					"open": null,
-					"close": null
-				}
-			},
-			"email": null,
-			"description": null,
-			"country": "USA",
-			"city": "Bear",
-			"category": "Wines",
-			"address_line_2": null,
-			"address_line_1": "2626 Chipmunk Court"
-		}]
-	};
+				"email": null,
+				"description": null,
+				"country": "USA",
+				"city": "Bear",
+				"category": "Wines",
+				"address_line_2": null,
+				"address_line_1": "2626 Chipmunk Court"
+			}]
+		};
 		this.state = {
             showList: true,
             businesses: json.data,
